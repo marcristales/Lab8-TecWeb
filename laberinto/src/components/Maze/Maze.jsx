5e5a65c47f40d8a71833dca80bff2f8d041a6bd2
Cristales <sirsly073@gmail.com>
2023-05-19 00:16:04 -0600

    Wall CSS Mod

--- laberinto/src/components/Maze/Maze.jsx
+++ laberinto/src/components/Maze/Maze.jsx
@@ -1,17 +1,103 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Maze.module.css'
-import useMazeData from '../../hooks/useMazeData'
+import Floor from '../Floor/Floor'
+import Wall from '../Wall/Wall'
+import Player from '../Player/Player'
 
 const Maze = () => {
-  const mazeData = useMazeData()
+  const [mazeData, setMazeData] = useState([])
+
+
+  const fetchMaze = async () => {
+    const response = await fetch(
+      `https://maze.uvgenios.online/?type=json&w=5&h=5`
+    )
+    const data = await response.json()
+    setMazeData(data)
+  }
+
+  useEffect(() => {
+    fetchMaze()
+  }, [])
 
   return (
     <div className={styles.container}>
       <div className={styles.maze}>
-        <pre>{mazeData}</pre>
+        {mazeData.map((row, rowIndex) => (
+          <div key={rowIndex} className={styles.row}>
+            {row.map((cell, cellIndex) => {
+              if (cell === '+' || cell === '-' || cell === '|') {
+                return (
+                  <div key={`${rowIndex}-${cellIndex}`}>
+                    <Wall type="kitchen" />
+                  </div>
+                );
+              } else if (cell === 'p') {
+                return (
+                  <div key={`${rowIndex}-${cellIndex}`}>
+                    <Player skin="mouse" />
+                  </div>
+                );
+              } else if (cell === ' ') {
+                return (
+                  <div key={`${rowIndex}-${cellIndex}`}>
+                    <Floor type="kitchen" />
+                  </div>
+                );
+              } else if (cell === 'g') {
+                return (
+                  <div key={`${rowIndex}-${cellIndex}`}>
+                    <Floor type="goal" />
+                  </div>
+                );
+              } else {
+                return null; // Add handling for other cell values if necessary
+              }
+            })}
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
+  // return (
+  //   <div className={styles.Maze}>
+  //     {mazeData.map((row, i) => {
+  //       return (
+  //         <div key={i} className={styles.Maze__row}>
+  //           {row.map((cell, j) => {
+  //             if (cell === "+" || cell === "-" || cell === "|") {
+  //               return (
+  //                 <Wall
+  //                   key={j}
+  //                   type="kitchen"
+  //                 />
+  //               )
+  //             } else if (cell === " ") {
+  //               return (
+  //                 <Floor
+  //                   key={j}
+  //                   type="kitchen"
+  //                 />
+  //               )
+  //               // } else if (cell === "p") {
+  //               //   return (
+  //               //     <Character
+  //               //       key={j}
+  //               //       backgroundImage={prompt.character}
+  //               //     />
+  //               //   )
+  //             } else if (cell === "g") {
+  //               return <Floor key={j} type="kitchen" />
+  //             } else {
+  //               return null
+  //             }
+  //           })}
+  //         </div>
+  //       )
+  //     })}
+
+  //   </div>
+  // )
 }
 
 export default Maze
