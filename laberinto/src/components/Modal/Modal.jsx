962a40e9dba14da61c21ae86058d7402b3e70329
Cristales <sirsly073@gmail.com>
2023-05-20 17:43:18 -0600

    Modal Component

---
+++ laberinto/src/components/Modal/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Modal.module.css'
+
+const Modal = ({ children }) => {
+    return (
+        <div className={styles.modalOverlay}>
+            <div className={styles.modalContent}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Modal
---
+++ laberinto/src/components/Modal/Modal.stories.js
@@ -0,0 +1,13 @@
+import Modal from "./Modal";
+
+export default {
+    title: 'Menu/Modal',
+    component: Modal,
+    tags: ['autodocs'],
+}
+
+export const Modal1 = {
+    args: {
+        type: 'modal',
+    },
+}
