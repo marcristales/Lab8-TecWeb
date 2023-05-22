e74fb96709dbc435bb913303caa05ee302eaa8fe
Cristales <sirsly073@gmail.com>
2023-05-21 22:47:52 -0600

    Update Player.jsx

--- laberinto/src/components/Player/Player.jsx
+++ laberinto/src/components/Player/Player.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types"
 import styles from "./Player.module.css"
 
 const Player = ({ skin, direction }) => {
-    return <div className={`${styles.player} ${styles[`player--${skin}--${direction}`]}`} />
+  return <div className={`${styles.player} ${styles[`player--${skin}--${direction}`]}`} />
 }
 
 Player.propTypes = {
-    skin: PropTypes.oneOf(['astronaut', 'mouse', 'knight']),
-    direction: PropTypes.oneOf(['up', 'down', 'left', 'right'])
-  }
+  skin: PropTypes.oneOf(['astronaut', 'mouse', 'knight']),
+  direction: PropTypes.oneOf(['up', 'down', 'left', 'right'])
+}
 
 export default Player
